02e08c837dff3cf4a09001bab00e4839095e400c
Zoran G <68257343+verona-hub@users.noreply.github.com>
2022-04-11 18:33:08 +0200

    - Market Table 24h price change: added a red and green className to the displayed result to match if number is negative or positive

--- src/components/Main/Market/MarketTable/MarketTableItem.js
+++ src/components/Main/Market/MarketTable/MarketTableItem.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
 import { Table } from "semantic-ui-react";
+import axios from "axios";
 
 
 const MarketTableItem = ({ item }) => {
@@ -12,15 +13,22 @@ const MarketTableItem = ({ item }) => {
         circulating_supply
     } = item;
 
+    // Check if 24hr price change is positive or negative and add the according className
+    const numClass = Math.sign(price_change_percentage_24h) === -1 ? `negative-number` : `positive-number`;
+
+
 
     return (
-        <Table.Row>
+        <Table.Row className="MarketTableItem">
             <Table.Cell> {market_cap_rank} </Table.Cell>
             <Table.Cell className='name'> <img src={image} alt='coin logo'/> {name} </Table.Cell>
             <Table.Cell> {symbol.toUpperCase()} </Table.Cell>
             <Table.Cell> &#36;{current_price} </Table.Cell>
             <Table.Cell> &#36;{market_cap} </Table.Cell>
-            <Table.Cell> {price_change_percentage_24h.toFixed(2)} &#37;</Table.Cell>
+            <Table.Cell
+                className={ numClass }>
+                {price_change_percentage_24h.toFixed(2)} &#37;
+            </Table.Cell>
             <Table.Cell> {circulating_supply} {item.symbol.toUpperCase()} </Table.Cell>
         </Table.Row>
     );
